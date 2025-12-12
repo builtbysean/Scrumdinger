c33c6aebf73779f91410b8d9a06bf8a9ce4287fe
Sean Coffin <seancoffin2003@gmail.com>
2025-12-12 15:54:25 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sean Coffin on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
